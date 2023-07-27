55f63cc34fb8efcfef97af11a48a328d98d77911
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-07-27 12:14:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,9 +12,14 @@ function allowDrop(event) {
 function dropped(event) {
 	console.log(event.target);
 	const temp_id = event.target.id;
+	const innerText1 = event.target.innerText;
+	//The replacing container
 	const data_id = event.dataTransfer.getData("text");
 	const container = document.getElementById(data_id);
+	const innerText2 = container.innerText;
 	container.id = temp_id;
+	container.innerText = innerText1;
 	event.target.id = data_id;
+	event.target.innerText = innerText2;
 	
 }
