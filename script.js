3abc675c96d8e14f6b69dbb8fa248cf4c3e0d5cb
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-07-27 12:08:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+
+function startDragging(event) {
+	event.dataTransfer.setData("text" , event.target.id);
+}
+
+function allowDrop(event) {
+	event.preventDefault();
+}
+
+
+function dropped(event) {
+	console.log(event.target);
+	const temp_id = event.target.id;
+	const data_id = event.dataTransfer.getData("text");
+	const container = document.getElementById(data_id);
+	container.id = temp_id;
+	event.target.id = data_id;
+	
+}
